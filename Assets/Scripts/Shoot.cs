07e036e12f89b8ab56d93c55a6f4ee843007028a
Alexander <allimon315@gmail.com>
2015-11-28 02:01:06 +0000

    Added a pistol that shoots, need to make it deal damage

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	public Rigidbody projectile;
+	public Transform barrelEnd;
+	public float speed = 20;
+
+	void Update () {
+		if (Input.GetButtonDown("Fire1")) {
+			Rigidbody instantiatedProjectile = Instantiate(projectile, barrelEnd.position, barrelEnd.rotation)as Rigidbody;
+			instantiatedProjectile.AddForce(barrelEnd.forward * -speed);
+		}
+	}
+}
