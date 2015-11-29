db1c5f95c705e185463294dd6fef5569a874771a
Alexander <allimon315@gmail.com>
2015-11-29 02:03:35 +0000

    Did gun recoil animation - glitchy

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -7,10 +7,25 @@ public class Shoot : MonoBehaviour {
 	public Transform barrelEnd;
 	public float speed = 20;
 
+	private AudioSource gunshot;
+	private Animator anim;
+	private bool shooting = false;
+
+	void Awake(){
+		gunshot = GetComponent<AudioSource> ();
+		anim = GetComponent<Animator> ();
+	}
+
 	void Update () {
 		if (Input.GetButtonDown("Fire1")) {
+			shooting = true;
+			Debug.Log(shooting);
 			Rigidbody instantiatedProjectile = Instantiate(projectile, barrelEnd.position, barrelEnd.rotation)as Rigidbody;
 			instantiatedProjectile.AddForce(barrelEnd.forward * -speed);
+			gunshot.Play();
+			anim.SetBool("shooting", shooting);
+			shooting = false;
+			Debug.Log(shooting);
 		}
 	}
 }
