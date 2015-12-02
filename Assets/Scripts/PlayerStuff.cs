2f5426c5e24601394bc4035a2c6d4733dfe06ee9
Alexander <allimon315@gmail.com>
2015-12-02 22:20:33 +0000

    Made cheap ass inventory system that works

--- Assets/Scripts/PlayerStuff.cs
+++ Assets/Scripts/PlayerStuff.cs
@@ -4,11 +4,19 @@ using System.Collections;
 public class PlayerStuff : MonoBehaviour {
 
 	public float health = 100;
+	public GameObject baseballBat;
+	public GameObject handgun;
 
+	private GameObject equipped;
 	private bool hasBaseballBat = false;
 	private int x = Screen.width / 2;
 	private int y = Screen.height / 2;
-	private bool baseballBat = false;
+
+	void Start(){
+		baseballBat.SetActive (true);
+		equipped = baseballBat;
+	}
+
 	void Update(){
 		RaycastHit hit;
 		if (Physics.Raycast (Camera.main.ScreenPointToRay (new Vector3 (x, y)), out hit)) {
@@ -19,10 +27,22 @@ public class PlayerStuff : MonoBehaviour {
 				}
 			}
 		}
+
+		//Cheap ass "inventory system"
+		if (Input.GetKeyDown(KeyCode.Alpha1) && baseballBat.activeSelf == false) {
+			equipped.SetActive(false);
+			baseballBat.SetActive(true);
+			equipped = baseballBat;
+		}
+		else if (Input.GetKeyDown(KeyCode.Alpha2) && handgun.activeSelf == false) {
+			equipped.SetActive(false);
+			handgun.SetActive(true);
+			equipped = baseballBat;
+		}
+
 	}
 
 	public void GainDamage(float damage){
 		health -= damage;
-		Debug.Log (health);
 	}
 }
